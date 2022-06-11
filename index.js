27705df03cc0e335ea40811114068635ff5c6200
cahito <cahito@gmail.com>
2022-06-10 21:12:13 -0300

    Feat - Requisito dois cumprido

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOTFOUND_STATUS = 404;
+const NOTFOUND_MESSAGE = 'Pessoa palestrante não encontrada';
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -17,6 +19,16 @@ app.get('/talker', async (_request, response) => {
   const data = await fs.readFile('./talker.json');
   const talkers = JSON.parse(data);
   response.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', async (request, response) => {
+  const { id } = request.params;
+  const data = await fs.readFile('./talker.json');
+  const talkers = JSON.parse(data);
+  const talker = talkers.find((t) => t.id === Number(id));
+  if (!talker) return response.status(HTTP_NOTFOUND_STATUS).json({ message: NOTFOUND_MESSAGE });
+
+  response.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
